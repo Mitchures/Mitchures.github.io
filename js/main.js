5e82fb1a846371bb790129b7ed6237d99f1c7a90
Mitchell Hollander <MitchellSH@live.com>
2014-07-03 18:38:34 -0500

    added random image loads on page from array

--- js/main.js
+++ js/main.js
@@ -8,12 +8,29 @@
 
   function initialize(){
     $('.options').click(fadeOut);
+    randomImage();
 
   }
 
   function fadeOut(){
     $('header').fadeOut('fast');
   }
+
+  function randomImage(){
+
+    var imageArray = new Array();
+
+    imageArray[0] = "img/zilla1.png";
+    imageArray[1] = "img/zilla2.png";
+    imageArray[2] = "img/zilla4.png";
+
+    var rand = Math.floor(Math.random()*3);
+
+    var imgPath = "<img class='animated fadeInDown' src='"+imageArray[rand]+"'/>";
+
+    document.getElementById("image-target").innerHTML = imgPath;
+
+}
 
 
 
