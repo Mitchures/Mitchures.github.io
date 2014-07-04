4cc86bfe4684507aced7da8775f1f09a8f37185c
Mitchell Hollander <MitchellSH@live.com>
2014-07-04 15:36:04 -0500

    added more images, icons and such

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@
 
 
   function initialize(){
-    $('.options').click(fadeOut);
     randomImage();
-
+    randomGreeting();
+    $('.options').click(fadeOut);
   }
 
   function fadeOut(){
@@ -22,15 +22,33 @@
 
     imageArray[0] = "img/zilla1.png";
     imageArray[1] = "img/zilla2.png";
-    imageArray[2] = "img/zilla4.png";
+    imageArray[2] = "img/firefly2.png";
+    imageArray[3] = "img/halo1.png";
 
-    var rand = Math.floor(Math.random()*3);
+    var rand = Math.floor(Math.random()*4);
 
     var imgPath = "<img class='animated fadeInDown' src='"+imageArray[rand]+"'/>";
 
     document.getElementById("image-target").innerHTML = imgPath;
 
 }
+
+  function randomGreeting(){
+    var greetingArray = new Array();
+
+    greetingArray[0] = 'Konnichiwa! Thats Japanese for Hello!';
+    greetingArray[1] = 'Aloha! Thats Hawaiian for Hello!';
+    greetingArray[2] = 'Hello There! Welcome!';
+
+    var random = Math.floor(Math.random()*3);
+
+    var greetPath = "<h2>"+greetingArray[random]+"</h2>";
+
+    document.getElementById("greeting").innerHTML = greetPath;
+  }
+
+
+
 
 
 
